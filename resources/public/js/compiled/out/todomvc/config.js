2650aecbed03d9b875e81f9998ad20ab1996b780
Brian Sunter <bsunter@gmail.com>
2018-02-09 18:53:57 -0800

    checkin

--- resources/public/js/compiled/out/todomvc/config.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.9.908 {}
-goog.provide('todomvc.config');
-goog.require('cljs.core');
-todomvc.config.debug_QMARK_ = goog.DEBUG;
-
-//# sourceMappingURL=config.js.map?rel=1517206659946
